b51faefa4c0537d2a06ce9fe0e3e49659c7ac47b
ilungachris <88008038+ilungachris@users.noreply.github.com>
2025-10-04 16:42:27 +0200

    Update theme.ts

--- app/theme.ts
+++ app/theme.ts
@@ -1,1 +1,15 @@
-
+// /app/theme.ts
+export const theme = {
+  colors: {
+    primaryDark: '#174D7D',
+    primary: '#2161AD',
+    accent: '#03B0DE',
+    gold: '#E0A33D',
+    white: '#FFFFFF',
+    bg: '#FFFFFF',
+    border: '#E5E7EB',
+    subtle: '#F8FAFC',
+  },
+  radius: 10,
+  pad: 12,
+};
